068c51ab534e11b60ecb89062685baf7f4947cdb
lalin <lalinpye@gmail.com>
2024-02-25 20:50:49 +0700

    TR-report

--- frontend/src/router/routes.js
+++ frontend/src/router/routes.js
@@ -3,11 +3,13 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../components/Home.vue';
 import BloodChecklist from '../components/allergy_blood/BloodChecklist.vue';
 import TransfusionForm from '../components/allergy_blood/TransfusionForm.vue';
+import TransfusionReport from '../components/allergy_blood/TransfusionReport.vue';
 import MainPage from '../components/mainPage/MainPage.vue';
 
 const routes = [
   { path: '/', component: BloodChecklist, name: 'blood-checklist' },
   { path: '/transfusion-form', component: TransfusionForm, name: 'transfusion-form' },
+  { path: '/transfusion-report', component: TransfusionReport, name: 'transfusion-report' },
   { path: '/mainBloodChecklist', component: MainPage, name: 'MainPage' },
   // Add more routes as needed
 ];
